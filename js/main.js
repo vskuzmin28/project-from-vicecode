567ec2223191d718c987d9ada2ec04b504f3fd38
vskuzmin28 <vskuzmin28@gmail.com>
2020-04-17 13:59:56 +0300

    added project rem-tv

--- js/main.js
+++ js/main.js
@@ -1,64 +1,214 @@
-(function ($) {
-  	$(document).ready(function () {
+$(function() {
+    //REWIEWS SLIDER
+    $('.rewiews-slider').slick({
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        responsive: [{
+                breakpoint: 900,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true
 
-  		$('input[name="phone"]').inputmask("+7 (999)-999-99-99");
+                }
+            },
+            {
+                breakpoint: 700,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ],
+        prevArrow: '<img class="slider arrows slider-arrows__left" src="./img/arrow-left.png" alt=""></img>',
+        nextArrow: '<img class="slider arrows slider-arrows__right"  src="./img/arrow-right.png" alt=""></img>',
+    });
 
-		var mainslider = new Swiper('.reviews-slider', {
-			slidesPerView: 1,
-			navigation: {
-				prevEl: '.swiper-button-prev',
-				nextEl: '.swiper-button-next',
-			},
-  		});
+    //BURGER MENU
+    $(function() {
+        $('.btn-menu').on('click', function() {
+            $('.menu').addClass('active')
+        });
 
-		$('.thanks-open').on ('click', function (e) {
-			e.preventDefault ();
-			$('.popup').modal('hide');
-			$('#thanks').modal('show');
-		} );
+        $('.menu__links-item').on('click', function() {
+            $(this).closest('.menu')
+                .removeClass('active');
+        });
+        $('.btn-close').on('click', function() {
+            $('.menu').removeClass('active')
+        });
+    });
 
-		$('.thanks-close').on ('click', function (e) {
-			e.preventDefault ();
-			$('.popup').modal('hide');
-		});
+    //PHONE MASK
+    $(document).ready(function() {
+        $('#phone').mask('+7 (999) 99-99-999');
+    });
+    $(document).ready(function() {
+        $('#popupPhone').mask('+7 (999) 99-99-999');
+    });
 
-		$('.mobile-menu').click(function(e){
-			e.preventDefault();
 
-			$(this).next().slideToggle(200);
-			$(this).toggleClass('active');
-		});
 
-		$('.header-city li').click(function(){
-			let text = $(this).text();
-				$('.header-city__result').text(text);
-		});
+    //MAIN POP-UP
+    // open
+    $('.js-button-campaign').click(function() {
+        $('.js-overlay-campaign').fadeIn();
+        $('.js-overlay-campaign').addClass('disabled');
+    });
+    // close popup
+    $('.js-close-campaign').click(function() {
+        $('.js-overlay-campaign').fadeOut();
 
-  		$(window).scroll(function(){
+    });
+    // close popup windows out
+    $(document).mouseup(function(e) {
+        var popup = $('.js-popup-campaign');
+        if (e.target != popup[0] && popup.has(e.target).length === 0) {
+            $('.js-overlay-campaign').fadeOut();
+        }
+    });
+    $('.send').click(function() {
+        $('.popup-form').removeClass();
+        $('.popup-final').addClass();
+    });
+    $('.close').click(function() {
+        $('.overlay').fadeOut();
 
-  			if($(window).scrollTop() > 180){
-  				$('.fixed-menu').slideDown(200);
-  			} else {
-				$('.fixed-menu').slideUp(200);
-  			}
+    });
 
-  		});
+    //START SALE FORM 
+    $(document).ready(function() {
+        $('#sale-contact__form').submit(function() {
+            if (document.form.salePhone.value == '') {
+                valid = false;
+                return valid;
+            }
+            $.ajax({
+                type: "POST",
+                url: "",
+                data: $(this).serialize()
+            }).done(function() {
 
-		$(".fixed-menu, .menu").on("click","a", function (event) {
-			
-			var id  = $(this).attr('href').split('#')[1];
+                $('.overlay-sale').fadeIn();
 
-			if ($('.'+id).length != 0) {
-				event.preventDefault();
-				var	top = $('.'+id).offset().top - 100;
+                $(this).find('input').val('');
+                $('#sale-contact__form').trigger('reset');
+            });
+            return false;
+        });
+    });
+    $('.close-popup').click(function() {
+        $('.overlay-sale').fadeOut();
+    });
+    $(document).mouseup(function(e) {
+        var popup = $('.popup-sale');
+        if (e.target != popup[0] && popup.has(e.target).length === 0) {
+            $('.overlay-sale').fadeOut();
+        }
+    });
+    $(document).ready(function() {
+        $('#salePhone').mask('+7 (999) 99-99-999');
+    });
 
-				$('body,html').animate({scrollTop: top}, 1500);
-			}
-		});
+    //START REPAIRS FORM
+    $(document).ready(function() {
+        $('#repairs-contact__form').submit(function() {
+            if (document.form.repairsPhone.value == '' || document.form.repairsText.value == '') {
+                valid = false;
+                return valid;
+            }
+            $.ajax({
+                type: "POST",
+                url: "",
+                data: $(this).serialize()
+            }).done(function() {
 
-		setTimeout(function(){
-			$('.contacts-map__menu').before('<script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Acf1526328c862ada7518996a9e84c0076b0e48f1c30d5e2d395098d0ecfd7f79&amp;width=100%25&amp;height=475&amp;lang=ru_RU&amp;scroll=true"></script>');
-		}, 4000);
+                $('.overlay-repairs').fadeIn();
 
-  	});
-})(jQuery);
+                $(this).find('input').val('');
+                $('#repairs-contact__form').trigger('reset');
+            });
+            return false;
+        });
+    });
+    $('.close-popup__repairs').click(function() {
+        $('.overlay-repairs').fadeOut();
+    });
+    $(document).mouseup(function(e) {
+        var popup = $('.popup-repairs');
+        if (e.target != popup[0] && popup.has(e.target).length === 0) {
+            $('.overlay-repairs').fadeOut();
+        }
+    });
+    $(document).ready(function() {
+        $('#repairsPhone').mask('+7 (999) 99-99-999');
+    });
+    //END REPAIRS FORM
+
+
+    //START FOOTER FORM
+    $(document).ready(function() {
+        $('#footer-contact__form').submit(function() {
+            if (document.form.footerPhone.value == '' || document.form.footerName.value == '' || document.form.footerComment.value == '') {
+                valid = false;
+                return valid;
+            }
+            $.ajax({
+                type: "POST",
+                url: "",
+                data: $(this).serialize()
+            }).done(function() {
+                $('.overlay-footer').fadeIn();
+                $(this).find('input').val('');
+                $('#footer-contact__form').trigger('reset');
+            });
+            return false;
+        });
+    });
+    $('.close-popup').click(function() {
+        $('.overlay-footer').fadeOut();
+    });
+    $(document).mouseup(function(e) {
+        var popup = $('.popup-repairs');
+        if (e.target != popup[0] && popup.has(e.target).length === 0) {
+            $('.overlay-footer').fadeOut();
+        }
+    });
+    $(document).ready(function() {
+        $('#footerPhone').mask('+7 (999) 99-99-999');
+    });
+
+    // float header
+
+    $('.header_float').hide();
+
+    function toggleHeader() {
+        let scroll_status = $(document).scrollTop();
+        let ww = $(window).width();
+        if (scroll_status > $(".header-navigation").height() && ww > 1000) {
+          $(".header_float").fadeIn();
+        }
+        else {
+            $(".header_float").fadeOut();
+        }
+      }
+
+      $(document).scroll(function () {
+        toggleHeader();
+      })
+
+      function toggleFirstHeader() {
+        let ww = $(window).width();
+        if (ww < 1000) {
+          $(".header-container").addClass('header_fixed');
+        }
+        else {
+            $(this).removeClass('header_fixed');
+        }
+      }
+
+      $(document).ready(function() {
+        toggleFirstHeader();
+      })
+
+});
